479ea5b6b5cd423ad0a10baf1d585871d10cfc92
jianglei12138 <jianglei12138@gmail.com>
2016-03-20 10:06:27 +0800

    add android.mk & shared and static library

---
+++ README.md
@@ -0,0 +1,2 @@
+#libcms2
+Little cms is a color management library. Implements fast transforms between ICC profiles. It is focused on speed, and is portable across several platforms (MIT license)
---
+++ jni/Android.mk
@@ -0,0 +1,43 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := lcms 
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/
+
+
+LOCAL_SRC_FILES := \
+		cmscam02.c \
+		cmsgmt.c \
+		cmslut.c \
+		cmspack.c \
+		cmssm.c \
+		cmscgats.c \
+		cmshalf.c \
+		cmsmd5.c \
+		cmspcs.c \
+		cmstypes.c \
+		cmscnvrt.c \
+		cmsintrp.c \
+		cmsmtrx.c \
+		cmsplugin.c \
+		cmsvirt.c \
+		cmserr.c \
+		cmsio0.c \
+		cmsnamed.c \
+		cmsps2.c \
+		cmswtpnt.c \
+		cmsgamma.c \
+		cmsio1.c \
+		cmsopt.c \
+		cmssamp.c \
+		cmsxform.c 
+
+LOCAL_CFLAGS += \
+		-DHAVE_CONFIG_H \
+
+include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
+
+
